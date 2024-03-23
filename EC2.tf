df1be4bf69eb04eec05c3f3a6ffd3c2e5ef9c22e
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-03-23 19:48:41 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "ecomm" {
-  ami                         = "ami-0c7f9161f8491665f"
+resource "aws_instance" "wordpress" {
+  ami                         = "ami-01387af90a62e3c01"
   instance_type               = "t2.micro"
   key_name                    = "WP"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
@@ -7,19 +7,7 @@ resource "aws_instance" "ecomm" {
   associate_public_ip_address = true
   user_data                   = file("data1.sh")
   tags = {
-    name = "EC2-1"
+    name = "wordpress"
   }
 }
 
-resource "aws_instance" "food" {
-  ami                         = "ami-0c7f9161f8491665f"
-  instance_type               = "t2.micro"
-  key_name                    = "WP"
-  vpc_security_group_ids      = [aws_security_group.web-sg.id]
-  subnet_id                   = aws_subnet.public-subnet2.id
-  associate_public_ip_address = true
-  user_data                   = file("data2.sh")
-  tags = {
-    name = "EC2-2"
-  }
-}
