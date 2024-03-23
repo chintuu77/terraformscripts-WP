13e37ee43cba64980f83fb847eb0e7cbe0f9cb3d
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-03-23 19:46:29 +0530

    Update data1.sh

--- data1.sh
+++ data1.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sudo yum update
-sudo yum -y install httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo yum -y install git
-git clone https://github.com/chintuu77/ecomm.git
-sudo mv ecomm/* /var/www/html/
-sudo systemctl restart httpd
+sudo yum -y install git 
+sudo yum -y install docker 
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo chmod 666 /var/run/docker.sock
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/chintuu77/wordpress.git
+cd wordpress
+docker-compose up -d
